ac90b56b91d559940031486a1e013cb82b938d05
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-10-15 15:36:23 -0700

    CRUD

---
+++ MarketUtilities-V2/Controllers/PasilloesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MarketUtilities_V2.Data;
+using MarketUtilities_V2.Models;
+
+namespace MarketUtilities_V2.Controllers
+{
+    public class PasilloesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PasilloesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pasilloes
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Pasillo.ToListAsync());
+        }
+
+        // GET: Pasilloes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Pasillo == null)
+            {
+                return NotFound();
+            }
+
+            var pasillo = await _context.Pasillo
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pasillo == null)
+            {
+                return NotFound();
+            }
+
+            return View(pasillo);
+        }
+
+        // GET: Pasilloes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pasilloes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,nombre,modified_by,created_by,moified_date")] Pasillo pasillo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pasillo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pasillo);
+        }
+
+        // GET: Pasilloes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Pasillo == null)
+            {
+                return NotFound();
+            }
+
+            var pasillo = await _context.Pasillo.FindAsync(id);
+            if (pasillo == null)
+            {
+                return NotFound();
+            }
+            return View(pasillo);
+        }
+
+        // POST: Pasilloes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,nombre,modified_by,created_by,moified_date")] Pasillo pasillo)
+        {
+            if (id != pasillo.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pasillo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PasilloExists(pasillo.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pasillo);
+        }
+
+        // GET: Pasilloes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Pasillo == null)
+            {
+                return NotFound();
+            }
+
+            var pasillo = await _context.Pasillo
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pasillo == null)
+            {
+                return NotFound();
+            }
+
+            return View(pasillo);
+        }
+
+        // POST: Pasilloes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Pasillo == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Pasillo'  is null.");
+            }
+            var pasillo = await _context.Pasillo.FindAsync(id);
+            if (pasillo != null)
+            {
+                _context.Pasillo.Remove(pasillo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PasilloExists(int id)
+        {
+          return _context.Pasillo.Any(e => e.id == id);
+        }
+    }
+}
---
+++ MarketUtilities-V2/Migrations/20221015221443_initial.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketUtilities_V2.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pasillo",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    modified_by = table.Column<int>(type: "int", nullable: false),
+                    created_by = table.Column<int>(type: "int", nullable: false),
+                    moified_date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pasillo", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    modified_by = table.Column<int>(type: "int", nullable: false),
+                    created_by = table.Column<int>(type: "int", nullable: false),
+                    moified_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    pasilloIDid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Categoria_Pasillo_pasilloIDid",
+                        column: x => x.pasilloIDid,
+                        principalTable: "Pasillo",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Producto",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    sku = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    codigoDeBarras = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    categoriaIDid = table.Column<int>(type: "int", nullable: false),
+                    modified_by = table.Column<int>(type: "int", nullable: false),
+                    moified_date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producto", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Producto_Categoria_categoriaIDid",
+                        column: x => x.categoriaIDid,
+                        principalTable: "Categoria",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categoria_pasilloIDid",
+                table: "Categoria",
+                column: "pasilloIDid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producto_categoriaIDid",
+                table: "Producto",
+                column: "categoriaIDid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Producto");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+
+            migrationBuilder.DropTable(
+                name: "Pasillo");
+        }
+    }
+}
